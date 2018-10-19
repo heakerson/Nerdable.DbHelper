a0d2239b5042ac6354f1fd2439e8a0b7743b0d3a
Heather Akerson <HeatherA@magenic.com>
2018-10-19 13:36:46 -0500

    Configuring the dbContext in the IServiceCollection extension method

--- Extensions/DbHelperExtensions.cs
+++ Extensions/DbHelperExtensions.cs
@@ -8,8 +8,12 @@ namespace Nerdable.DbHelper.Extensions
 {
     public static class DbHelperExtensions
     {
-        public static IServiceCollection AddDbHelper<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
+        public static IServiceCollection AddDbHelper<TDbContext>(this IServiceCollection services, string connection) where TDbContext : DbContext
         {
+            services.AddDbContext<TDbContext>(options =>
+                options.UseSqlServer(connection)
+            );
+
             services.AddScoped<IDbHelper, Services.DbHelper>();
             services.AddScoped<DbContext, TDbContext>();
             services.AddAutoMapper();
